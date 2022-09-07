21cc6cf85a0ac37d23f85ca4685f9f55f67fa411
ivansing <lancedealway@gmail.com>
2022-09-07 12:48:34 -0500

    firebase setup already done

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:ecommerce_app/config/app_router.dart';
 import 'package:ecommerce_app/config/theme.dart';
 
 import 'package:ecommerce_app/screens/splash/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -13,7 +14,9 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
